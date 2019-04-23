a42e7386cc1cd17f3e658cb044c92e0711e7dc42
Wahyu Alif Aldika <aldika94wahyu@gmail.com>
2019-04-23 21:17:50 +0700

    Tes API

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -47,7 +47,7 @@ class PassportController extends Controller
 
         if (auth()->attempt($credentials)) {
             $token = auth()->user()->createToken('TutsForWeb')->accessToken;
-            return response()->json(['token' => $token], 200);
+            return response()->json(['token' => $token, "sdfs" => "sdfds"], 200);
         } else {
             return response()->json(['error' => 'UnAuthorised'], 401);
         }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
